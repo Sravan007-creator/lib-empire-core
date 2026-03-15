98111abdbd61cf42e1fc29ca712b36c557f0d5db
nidin-cyber <nidin@empireoe.com>
2026-03-15 18:25:48 +0530

    feat: export service auth functions

--- empireoe_core/auth/__init__.py
+++ empireoe_core/auth/__init__.py
@@ -6,6 +6,7 @@ from empireoe_core.auth.security import (
     validate_password_complexity,
 )
 from empireoe_core.auth.rbac import require_roles
+from empireoe_core.auth.service import create_service_token, verify_service_token
 
 __all__ = [
     "create_access_token",
@@ -14,4 +15,7 @@ __all__ = [
     "verify_password",
     "validate_password_complexity",
     "require_roles",
+    "create_service_token",
+    "verify_service_token",
 ]
+
